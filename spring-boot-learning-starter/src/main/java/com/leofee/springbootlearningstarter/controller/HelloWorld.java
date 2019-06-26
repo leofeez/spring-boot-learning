00be375f3f948acb07a73cc5bbdc442cb7780ed6
lixiufei <lixiufei@ec.e-chemchina.com>
2019-06-26 11:14:43 +0800

    #2 增加base模块

--- spring-boot-learning-starter/src/main/java/com/leofee/springbootlearningstarter/controller/HelloWorld.java
+++ spring-boot-learning-starter/src/main/java/com/leofee/springbootlearningstarter/controller/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.leofee.springbootlearningstarter.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
