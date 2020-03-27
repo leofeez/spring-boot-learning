a3fc417558fb9a9287b35297b805c05314fdcbd0
lixiufei <mr.leofee@outlook.com>
2020-03-27 17:19:59 +0800

    spring-boot-learning-spi

---
+++ spring-boot-learning-spi/src/main/java/com/leofee/springlearningspi/SpringBootLearningSpiApplication.java
@@ -0,0 +1,12 @@
+package com.leofee.springlearningspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningSpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningSpiApplication.class, args);
+    }
+}
