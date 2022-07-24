f769c51f402f23010d646af62de8ff916b34e52b
leofee <mr.leofee@outlook.com>
2022-07-24 23:45:56 +0800

    rocket mq

--- spring-boot-learning-mq/src/main/java/com/leofee/rocketmq/README.md
+++ spring-boot-learning-mq/src/main/java/com/leofee/rocketmq/README.md
@@ -32,10 +32,12 @@
 
 ## 发送消息
 
+### 同步阻塞发送消息
+
 ```java
-    // producer
-    DefaultMQProducer producer=new DefaultMQProducer("hello_world_group");
-    // 指定 name server
+    // 1. 设置生产者组 producer
+    DefaultMQProducer producer=new DefaultMQProducer("hello_world_producer_group");
+    // 2. 指定 name server
     producer.setNamesrvAddr("192.168.248.131:9876");
     Message message=new Message("hello_world","hello rocketmq".getBytes());
     producer.start();
@@ -43,4 +45,106 @@
     SendResult sendResult=producer.send(message);
     System.out.println(sendResult);
 ```
-批量发送，`producer.send` 支持批量发送
+
+### 批量发送消息
+```java
+    // 批量发送
+    List<Message> messageList = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+        messageList.add(new Message("test_tag", "TAG-B", ("hello rocketmq" + i).getBytes()));
+    }
+    SendResult sendBatch = producer.send(messageList);
+```
+
+### 异步发送
+```java
+    // 异步发送
+    producer.send(new Message("hello_world", "hello rocketmq async".getBytes(StandardCharsets.UTF_8)), new SendCallback() { 
+        @Override
+        public void onSuccess(SendResult sendResult) {
+            System.out.println("async send result: " + sendResult);
+        }   
+        @Override
+        public void onException(Throwable e) {
+            e.printStackTrace();
+        }
+    });
+```
+
+## 消费消息
+
+```java
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
+    // Name Server
+    consumer.setNamesrvAddr("192.168.248.131:9876");
+    // 消费者组
+    consumer.setConsumerGroup("hello_world_consumer_group");
+    // * 代表接收该Topic 下的所有消息
+    consumer.subscribe("hello_world", "*");
+    // 注册消息监听
+    consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+        for (MessageExt msg : msgs) {   System.out.println(new String(msg.getBody()));
+        }
+        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+    }); 
+    // 开始接收消息
+    consumer.start();
+```
+
+## 消息过滤
+
+1. 通过 `Tag` 过滤消息
+
+生产者生产消息时指定消息的具体`Tag`
+```java
+    DefaultMQProducer producer = new DefaultMQProducer("hello_world_producer_group");
+    producer.setNamesrvAddr("192.168.248.131:9876");
+    producer.start();
+    // Message 的 构造方法支持指定 Tag
+    Message message = new Message("test_tag", "TAG-A", "hello world rocket tag filter".getBytes());
+    SendResult sendResult = producer.send(message);
+```
+
+消费者消费消息时，指定对应的过滤策略
+```java
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
+    consumer.setNamesrvAddr("192.168.248.131:9876");
+    consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> { 
+        for (MessageExt msg : msgs) {
+            System.out.println(new String(msg.getBody()));
+        }   
+        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+    });
+    // 指定只处理指定Tag的消息
+    consumer.subscribe("test_tag", "TAG-A");
+    consumer.start();
+```
+2. 通过 SQL 方式过滤消息
+要使用SQL模式进行消息过滤，需要在RocketMQ的conf/broker.xml 中设置`enableProteryFilter=true`
+
+生产者生产消息时设置对应的 Property
+```java
+    DefaultMQProducer producer = new DefaultMQProducer("hello_world_producer_group");
+    producer.setNamesrvAddr("192.168.248.131:9876");
+    producer.start();   
+    // 批量发送 List<Message> messageList = new ArrayList<>();  for (int i = 0; i < 5; i++) {
+            Message message = new Message("test_tag", "TAG-B", ("hello rocketmq" + i).getBytes());
+            // 设置对应的属性
+            message.putUserProperty("order", i + "");
+            messageList.add(message);
+    }
+    SendResult sendBatch = producer.send(messageList);
+```
+
+消费者消费消息：
+```java
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
+    consumer.setNamesrvAddr("192.168.248.131:9876");
+    consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> { 
+        for (MessageExt msg : msgs) {
+            System.out.println(new String(msg.getBody()));
+        }   
+        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+    });
+    // 创建对应的过滤器 MessageSelector selector = MessageSelector.bySql("order > 5 and order <= 10 "); consumer.subscribe("test_tag", selector);   consumer.start();
+```
--- spring-boot-learning-mq/src/test/java/com/leofee/rocketmq/RocketMqClientTest.java
+++ spring-boot-learning-mq/src/test/java/com/leofee/rocketmq/RocketMqClientTest.java
@@ -1,18 +1,23 @@
 package com.leofee.rocketmq;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MessageSelector;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author leofee
@@ -21,13 +26,13 @@ public class RocketMqClientTest {
 
     @Test
     public void producer() throws Exception {
-        DefaultMQProducer producer = new DefaultMQProducer("hello_world_group");
+        DefaultMQProducer producer = new DefaultMQProducer("hello_world_producer_group");
         producer.setNamesrvAddr("192.168.248.131:9876");
         producer.start();
         Message message = new Message("hello_world", "hello rocketmq".getBytes());
         // 同步消息发送
         SendResult sendResult = producer.send(message);
-        System.out.println(sendResult);
+        System.out.println("send result: " + sendResult);
 
         // 批量发送
         List<Message> messageList = new ArrayList<>();
@@ -35,14 +40,14 @@ public class RocketMqClientTest {
             messageList.add(new Message("hello_world", ("hello rocketmq" + i).getBytes()));
         }
         SendResult sendBatch = producer.send(messageList);
-        System.out.println(sendBatch);
+        System.out.println("batch send result :" + sendBatch);
 
         // 异步发送
         producer.send(new Message("hello_world", "hello rocketmq async".getBytes(StandardCharsets.UTF_8)), new SendCallback() {
 
             @Override
             public void onSuccess(SendResult sendResult) {
-                System.out.println("async send result" + sendResult);
+                System.out.println("async send result: " + sendResult);
             }
 
             @Override
@@ -51,12 +56,14 @@ public class RocketMqClientTest {
             }
         });
 
+        // 阻塞
+        new CountDownLatch(1).await();
         producer.shutdown();
     }
 
     @Test
     public void consumer() throws Exception {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer();
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
         consumer.setNamesrvAddr("192.168.248.131:9876");
         consumer.setConsumerGroup("hello_world_consumer_group");
         consumer.subscribe("hello_world", "*");
@@ -68,6 +75,86 @@ public class RocketMqClientTest {
         });
 
         consumer.start();
-        while(true) {}
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void producer02() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        DefaultMQProducer producer = new DefaultMQProducer("hello_world_producer_group");
+        producer.setNamesrvAddr("192.168.248.131:9876");
+        producer.start();
+
+        Message message = new Message("test_tag", "TAG-A", "hello world rocket tag filter".getBytes());
+        SendResult sendResult = producer.send(message);
+        System.out.println("producer02 send result: " + sendResult);
+
+        // 批量发送
+        List<Message> messageList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            messageList.add(new Message("test_tag", "TAG-B", ("hello rocketmq" + i).getBytes()));
+        }
+        SendResult sendBatch = producer.send(messageList);
+        System.out.println("producer02 send result: " + sendBatch);
+
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void consumer02() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
+        consumer.setNamesrvAddr("192.168.248.131:9876");
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+
+            for (MessageExt msg : msgs) {
+                System.out.println(new String(msg.getBody()));
+            }
+
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        consumer.start();
+        consumer.subscribe("test_tag", "TAG-A");
+
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void producer03() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        DefaultMQProducer producer = new DefaultMQProducer("hello_world_producer_group");
+        producer.setNamesrvAddr("192.168.248.131:9876");
+        producer.start();
+
+        // 批量发送
+        List<Message> messageList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Message message = new Message("test_selector", "TAG-B", ("hello rocketmq" + i).getBytes());
+            // 指定对应的属性
+            message.putUserProperty("order", i + "");
+            messageList.add(message);
+        }
+        SendResult sendBatch = producer.send(messageList);
+        System.out.println("producer02 send result: " + sendBatch);
+
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void consumer03() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("hello_world_consumer_group");
+        consumer.setNamesrvAddr("192.168.248.131:9876");
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+
+            for (MessageExt msg : msgs) {
+                System.out.println(new String(msg.getBody()));
+            }
+
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        // 创建对应的过滤器
+        MessageSelector selector = MessageSelector.bySql("order > 5 and order <= 10 ");
+        consumer.subscribe("test_selector", selector);
+        consumer.start();
+
+        new CountDownLatch(1).await();
     }
 }
